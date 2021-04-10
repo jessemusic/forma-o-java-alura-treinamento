253871cddbdffa28ee2f574d3908e44bfe169939
jessemusic <jmc_cursos@hotmail.com>
2021-04-10 12:48:50 -0300

    praticando o lambda

--- NovaFuncionalidade/src/java8/OrdenaStrings.java
+++ NovaFuncionalidade/src/java8/OrdenaStrings.java
@@ -6,8 +6,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
 
-
-
 public class OrdenaStrings {
 
 	public static void main(String[] args) {
@@ -16,50 +14,30 @@ public class OrdenaStrings {
 		palavras.add("jesse Caetano dos Santos");
 		palavras.add("matemática saraiva");
 		palavras.add("editora casa do codigo");
-		
-		Comparator<String> comp = new ComparadorPorTamanho();	
-		Collections.sort(palavras, comp);
-		
-		System.out.println(palavras);
-		
-		Consumer<String> consumidor = new Consumer<String>() {
 
-			@Override
-			public void accept(String s) {
+		Comparator<String> comp = new ComparadorPorTamanho();
+		Collections.sort(palavras, comp);
+
+		System.out.println(palavras);
+
+		palavras.forEach((String s) -> {
 			System.out.println(s);
-				
-			}
-		};
-		
-		palavras.forEach(consumidor);
+
+		});
 
 	}
-	
 
 }
- class ImprimeNaLinha implements Consumer<String>{
-
-	@Override
-	public void accept(String t) {
-		System.out.println(t);
-		
-	}
-	 
- }
-
-
-
-
 
 class ComparadorPorTamanho implements Comparator<String> {
 
 	@Override
 	public int compare(String s1, String s2) {
-		if(s1.length()<s2.length())
+		if (s1.length() < s2.length())
 			return -1;
-		if(s1.length()>s2.length())
+		if (s1.length() > s2.length())
 			return 1;
 		return 0;
 	}
-	
+
 }
